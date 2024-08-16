e6ad8eaa93bb81939ca881285b75e4c6ebabb8b2
Kunal Bhalerao <kunalbhalerao789@gmail.com>
2024-08-16 14:39:33 +0530

    changing image extension name in workflow file

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,5 +1,5 @@
 import { CheckCircle2 } from "lucide-react";
-import codeImg from "../assets/code.jpg";
+import codeImg from "../assets/code.webp";
 import { checklistItems } from "../constants";
 
 const Workflow = () => {
